ca9afdb2d77521d05642aab126ae0e9f33182e98
daniel <daniel.cainelli@buonarroti.tn.it>
2023-10-15 17:12:53 +0200

    creazione classe Pesci modifiche classe Pesci e test main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        String nomePesce1="Carlo";
+        String nomePesce2="Nino";
+        int etaPesce1=1;
+        int etaPesce2=2;
+        Pesci pesce1 = new Pesci(nomePesce1,etaPesce1);
+        Pesci pesce2 = new Pesci(nomePesce2,etaPesce2);
+        System.out.println(pesce1.getNome());
+        System.out.println(pesce1.getEta());
+        System.out.println(pesce1.toString());
+        System.out.println(pesce2.getNome());
+        System.out.println(pesce2.getEta());
+        System.out.println(pesce2.toString());
     }
 }
--- src/Pesci.java
+++ src/Pesci.java
@@ -1,18 +1,24 @@
 public class Pesci {
     private String nome;
+    private int eta;
     
-    public Pesci(String nome){
+    public Pesci(String nome, int eta){
         this.nome=nome;
+        this.eta=eta;
     }
     public String getNome(){
         return this.nome;
     }
+    public int getEta(){
+        return this.eta;
+    }
     public Pesci clone(){
-        return new Pesci(this.nome);
+        return new Pesci(this.nome, this.eta);
     }
     public String toString(){
         String rit="{\t";
         rit+="nome:\t"+this.nome;
+        rit+="eta`:\t"+this.eta;
         rit+="\t}";
         return rit;
     }
