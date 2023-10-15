5eddfc9c647ed36e87ea6fdaa3e00679bf675959
daniel <daniel.cainelli@buonarroti.tn.it>
2023-10-15 18:14:52 +0200

    creazione classe Pesci modifiche classe Pesci e test main verifica classe Pesci e modifica toString Pesci modifica e correzione metodo setPesce nella classe Acquario + creazione getPesce per array gia pieno e test di questi due metodi sul main creazione e test nel main dei metodi aggiungi e rimuovi pesce creazione toString, conclusione classe Acquario e test del progetto

--- src/Acquario.java
+++ src/Acquario.java
@@ -1,2 +1,71 @@
-public class acquario {
+public class Acquario {
+    private String forma;
+
+    private Pesci[] pesci;
+
+    public Acquario(int numeroPesci, String forma) {
+        this.forma = forma;
+        this.pesci = new Pesci[numeroPesci];
+    }
+
+    public Acquario(Pesci[] pesci, String forma) {
+        this.forma = forma;
+        this.pesci = pesci;
+    }
+
+    public String getForma() {
+        return forma;
+    }
+
+    public Pesci getPesci(int pos) {
+        Pesci rit = null;
+        if (pesci[pos] != null && pos >= 0 && pos < pesci.length) {
+            rit = pesci[pos];
+        }
+        return rit;
+    }
+
+    public void setPesci(int pos, String nome, int eta) {
+        if (pesci[pos] != null && pos >= 0 && pos < pesci.length) {
+            pesci[pos] = new Pesci(nome, eta);
+        }
+    }
+
+    public void aggiungiPesce(int pos, String nome, int eta) {
+        if (pesci[pos] == null && pos >= 0 && pos < pesci.length) {
+            pesci[pos] = new Pesci(nome, eta);
+        }
+    }
+
+    public void rimuoviPesce(int pos){
+        if(pesci[pos]!=null&&pos>=0&&pos<pesci.length){
+            pesci[pos] = null;
+        }
+    }
+
+    public String toString(){
+        String rit="{\t";
+        rit+="forma:\t"+this.forma;
+        rit+="\tpesci:\t";
+        for (int i = 0; i < this.pesci.length; i++) {
+            rit+="[\t"+this.pesci[i];
+        }
+        rit+="\t]";
+        rit+="\t}";
+        return rit;
+
+    }
+    public Acquario clone() {
+        Acquario acquario = new Acquario(new Pesci[this.pesci.length], this.forma);
+        for (int i = 0; i < this.pesci.length; i++) {
+            if (this.pesci[i] != null) {
+                acquario.pesci[i] = this.pesci[i].clone();
+            } else {
+                acquario.pesci[i] = null;
+            }
+        }
+        return acquario;
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         String nomePesce2="Nino";
         int etaPesce1=1;
         int etaPesce2=2;
+
         Pesci pesce1 = new Pesci(nomePesce1,etaPesce1);
         Pesci pesce2 = new Pesci(nomePesce2,etaPesce2);
         System.out.println(pesce1.getNome());
@@ -12,5 +13,29 @@ public class Main {
         System.out.println(pesce2.getNome());
         System.out.println(pesce2.getEta());
         System.out.println(pesce2.toString());
+
+
+        String formaAcquario1="rotondo";
+        String formaAcquario2="rettangolare";
+        Pesci[] pesci = {pesce1,pesce2};
+
+        Acquario acquario1 = new Acquario(pesci,formaAcquario1);
+        Acquario acquario2 = new Acquario(2,formaAcquario2);
+        System.out.println(acquario1.getForma());
+        System.out.println(acquario1.getPesci(0));
+        System.out.println(acquario1.getPesci(1));
+        acquario1.setPesci(0,"Luca",3);
+        System.out.println(acquario1.getPesci(0));
+        System.out.println(acquario2.getForma());
+        acquario2.aggiungiPesce(0,"Matteo",4);
+        acquario2.aggiungiPesce(1,"Silverio",1);
+        System.out.println(acquario2.getPesci(0));
+        System.out.println(acquario2.getPesci(1));
+        acquario2.rimuoviPesce(0);
+        acquario2.aggiungiPesce(0,"Rino",2);
+        System.out.println(acquario2.getPesci(0));
+        System.out.println(acquario2.clone());
+
+
     }
 }
